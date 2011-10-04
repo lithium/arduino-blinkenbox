09ae360ece957165c79cc27595010bef92d74bce
Wiggins <wiggins@concentricsky.com>
2011-10-04 14:02:46 -0700

    opcode stubs

--- blinkenboxinstance.cpp
+++ blinkenboxinstance.cpp
@@ -24,8 +24,174 @@ BlinkenBoxInstance::~BlinkenBoxInstance()
 {
 }
 
+
 void BlinkenBoxInstance::run()
 {
+    running_ = 1;
+    while (running_) {
+        run_one_instruction();
+        // scale speed?
+    }
+}
+
+void BlinkenBoxInstance::run_one_instruction()
+{
+    // fetch one instruction and adjust PC
+    byte *instruction = fetch_instruction_byte();
+
+    if (instruction == 0) {
+        return segfault(PC_);
+    }
+
+    //
+    // some opcodes are only 4 bits wide, test for those first
+    // 0xA*, 0xB*, 0xC*, 0xE*, 0x3*, 0x4*, 0x5*, 0x6*, 0x7*
+    byte opcode = (*instruction) & 0xF0;
+    if (opcode == 0xE0 || (opcode >= 0xA0 && opcode <= 0xC0 && opcode >= 0x30 && opcode <= 0x70)) {
+        byte d = (*instruction) & 0x0F;
+        byte *k = fetch_instruction_byte();
+        if (k == 0) { 
+            return segfault(PC_);
+        }
+
+        switch (opcode) {
+            case 0x30: //CPI
+                return opcode_cpi(d, *k);
+            case 0x40: //SBCI
+                return opcode_sbci(d, *k);
+            case 0x50: //SUBI
+                return opcode_subi(d, *k);
+            case 0x60: //ORI
+                return opcode_ori(d, *k);
+            case 0x70: //ANDI
+                return opcode_andi(d, *k);
+            case 0xA0: //LDS
+                return opcode_lds(d, *k);
+            case 0xB0: //LDI
+                return opcode_ldi(d, *k);
+            case 0xC0: //STS
+                return opcode_sts(d, *k);
+            case 0xE0: //SBR
+                return opcode_sbr(d, *k);
+            default:
+                return invalid_opcode(*instruction);
+        }
+    }
+
+
+    // these instructions all have no operand byte
+    switch (*instruction) {
+        case 0x00: // HALT
+            return opcode_halt();
+        case 0xFF: // NOP
+            return opcode_nop();
+        case 0xDD: // RET
+            return opcode_ret();
+        case 0xD9: // ICALL
+            return opcode_icall();
+        case 0xDB: // IJMP
+            return opcode_ijmp();
+    }
+
+    // remaining instructions all have an operand byte
+    byte *operand = fetch_instruction_byte();
+    byte d = (*operand & 0xF0) >> 4;
+    byte r = (*operand & 0x0F);
+
+    switch (*instruction) {
+        case 0x2C: // MOV
+            return opcode_mov(d, r);
+        case 0x80: // LD
+            return opcode_ld(d);
+        case 0x82: // ST
+            return opcode_st(d);
+        case 0x05: // PUSH
+            return opcode_push(d);
+        case 0x01: // POP
+            return opcode_pop(d);
+
+        case 0x09: // LSL
+            return opcode_lsl(d);
+        case 0x1D: // LSR
+            return opcode_lsr(d);
+        case 0x98: // ROL
+            return opcode_rol(d);
+        case 0x99: // ROR
+            return opcode_ror(d);
+        case 0x11: // ASR
+            return opcode_asr(d);
+        case 0x06: // BCLR
+            return opcode_bclr(r & 0x7);
+        case 0x07: // BSET
+            return opcode_bset(r & 0x7);
+
+        case 0x10: // CPSE
+            return opcode_cpse(d, r);
+        case 0x14: // CP
+            return opcode_cp(d, r);
+        case 0x04: // CPC
+            return opcode_cpc(d, r);
+        case 0xD0: // RCALL
+            return opcode_rcall(*operand);
+        case 0xD1: // CALL
+            return opcode_call(*operand);
+        case 0xD3: // RJMP
+            return opcode_rjmp(*operand);
+        case 0xD7: // JMP
+            return opcode_jmp(*operand);
+        case 0xF0: // BRCS
+            return opcode_brcs(*operand);
+        case 0xF1: // BREQ
+            return opcode_breq(*operand);
+        case 0xF2: // BRMI
+            return opcode_brmi(*operand);
+        case 0xF4: // BRLT
+            return opcode_brlt(*operand);
+        case 0xF8: // BRCC
+            return opcode_brcc(*operand);
+        case 0xF9: // BRNE
+            return opcode_brne(*operand);
+        case 0xFA: // BRPL
+            return opcode_brpl(*operand);
+        case 0xFC: // BRGE
+            return opcode_brge(*operand);
+        case 0xFD: // SBRC
+            return opcode_sbrc(d, r & 0x7);
+        case 0xFE: // SBRS
+            return opcode_sbrs(d, r & 0x7);
+
+        case 0x96: // DEC
+            return opcode_dec(d);
+        case 0x97: // INC
+            return opcode_inc(d);
+        case 0x9A: // MUL
+            return opcode_mul(d, r);
+        case 0x02: // MULS
+            return opcode_muls(d, r);
+        case 0x03: // MULSU
+            return opcode_mulsu(d, r);
+        case 0x08: // SBC
+            return opcode_sbc(d, r);
+        case 0x0C: // ADD
+            return opcode_add(d, r);
+        case 0x18: // SUB
+            return opcode_sub(d, r);
+        case 0x1C: // ADC
+            return opcode_adc(d, r);
+
+        case 0x20: // AND
+            return opcode_and(d, r);
+        case 0x24: // EOR
+            return opcode_eor(d, r);
+        case 0x28: // OR
+            return opcode_or(d, r);
+        case 0x91: // NEG
+            return opcode_neg(d);
+        case 0x94: // COM
+            return opcode_com(d);
+    }
+
+    return invalid_opcode(*instruction);
 }
 
 void *BlinkenBoxInstance::access_memory(int address)
@@ -57,4 +223,257 @@ void *BlinkenBoxInstance::access_system_memory(int offset)
 
 }
 
+byte *BlinkenBoxInstance::fetch_instruction_byte()
+{
+    byte *instruction = (byte *)access_memory(PC_);
+
+    if (instruction == 0) {
+        return 0;
+    }
+    PC_ += 1;
+    return instruction;
+}
+
+
+
+void BlinkenBoxInstance::invalid_opcode(byte instruction)
+{
+}
+void BlinkenBoxInstance::segfault(int address)
+{
+}
+
+
+
+/*
+ * opcode functions
+ *
+ */
+void BlinkenBoxInstance::opcode_cpi(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_sbci(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_subi(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_ori(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_andi(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_lds(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_ldi(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_sts(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_sbr(byte d, byte k)
+{
+}
+
+void BlinkenBoxInstance::opcode_halt()
+{
+}
+
+void BlinkenBoxInstance::opcode_nop()
+{
+}
+
+void BlinkenBoxInstance::opcode_ret()
+{
+}
+
+void BlinkenBoxInstance::opcode_icall()
+{
+}
+
+void BlinkenBoxInstance::opcode_ijmp()
+{
+}
+
+void BlinkenBoxInstance::opcode_mov(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_ld(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_st(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_push(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_pop(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_lsl(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_lsr(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_rol(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_ror(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_asr(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_bclr(byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_bset(byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_cpse(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_cp(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_cpc(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_rcall(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_call(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_rjmp(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_jmp(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brcs(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_breq(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brmi(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brlt(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brcc(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brne(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brpl(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_brge(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_sbrc(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_sbrs(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_dec(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_inc(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_mul(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_muls(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_mulsu(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_sbc(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_add(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_sub(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_adc(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_and(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_eor(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_or(byte d, byte r)
+{
+}
+
+void BlinkenBoxInstance::opcode_neg(byte d)
+{
+}
+
+void BlinkenBoxInstance::opcode_com(byte d)
+{
+}
 
--- blinkenboxinstance.h
+++ blinkenboxinstance.h
@@ -12,6 +12,8 @@ class BlinkenBoxInstance {
     void run();
     
   private:
+    bool running_;
+
     int SP_;
     int PC_;
     char R_[16];   // 16 8bit registers
@@ -25,6 +27,73 @@ class BlinkenBoxInstance {
     
     void *access_memory(int address);
     void *access_system_memory(int offset);
+    byte *fetch_instruction_byte();
+
+    void run_one_instruction();
+
+    void invalid_opcode(byte opcode);
+    void segfault(int address);
+
+
+
+
+    void opcode_cpi(byte d, byte k);
+    void opcode_sbci(byte d, byte k);
+    void opcode_subi(byte d, byte k);
+    void opcode_ori(byte d, byte k);
+    void opcode_andi(byte d, byte k);
+    void opcode_lds(byte d, byte k);
+    void opcode_ldi(byte d, byte k);
+    void opcode_sts(byte d, byte k);
+    void opcode_sbr(byte d, byte k);
+    void opcode_halt();
+    void opcode_nop();
+    void opcode_ret();
+    void opcode_icall();
+    void opcode_ijmp();
+    void opcode_mov(byte d, byte r);
+    void opcode_ld(byte d);
+    void opcode_st(byte d);
+    void opcode_push(byte d);
+    void opcode_pop(byte d);
+    void opcode_lsl(byte d);
+    void opcode_lsr(byte d);
+    void opcode_rol(byte d);
+    void opcode_ror(byte d);
+    void opcode_asr(byte d);
+    void opcode_bclr(byte r);
+    void opcode_bset(byte r);
+    void opcode_cpse(byte d, byte r);
+    void opcode_cp(byte d, byte r);
+    void opcode_cpc(byte d, byte r);
+    void opcode_rcall(byte d);
+    void opcode_call(byte d);
+    void opcode_rjmp(byte d);
+    void opcode_jmp(byte d);
+    void opcode_brcs(byte d);
+    void opcode_breq(byte d);
+    void opcode_brmi(byte d);
+    void opcode_brlt(byte d);
+    void opcode_brcc(byte d);
+    void opcode_brne(byte d);
+    void opcode_brpl(byte d);
+    void opcode_brge(byte d);
+    void opcode_sbrc(byte d, byte r);
+    void opcode_sbrs(byte d, byte r);
+    void opcode_dec(byte d);
+    void opcode_inc(byte d);
+    void opcode_mul(byte d, byte r);
+    void opcode_muls(byte d, byte r);
+    void opcode_mulsu(byte d, byte r);
+    void opcode_sbc(byte d, byte r);
+    void opcode_add(byte d, byte r);
+    void opcode_sub(byte d, byte r);
+    void opcode_adc(byte d, byte r);
+    void opcode_and(byte d, byte r);
+    void opcode_eor(byte d, byte r);
+    void opcode_or(byte d, byte r);
+    void opcode_neg(byte d);
+    void opcode_com(byte d);
     
 };
 
